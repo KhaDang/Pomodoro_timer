7c3ab0498b3360eb3b434970ada2688bae8ead0c
GarrickDang <dminhkha@gmail.com>
2023-07-19 08:19:38 +0700

    second commit and Push

--- main.py
+++ main.py
@@ -10,18 +10,42 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_mark.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
+    reps += 1
+
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-    count_down(5*60)
+    # If it's the 1st/3rd/5th/7th rep:
+
+    if reps % 8 == 0:
+        # If it's 8th rep:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        # If it's the 2nd/4th/6th rep:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count/60)
@@ -31,9 +55,16 @@ def count_down(count):
         count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
-    if count >0:
-        window.after(1000, count_down, count -1)
-
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count -1)
+    else:
+        start_timer()
+        marks = ""
+        for _ in range (math.floor(reps/2)):
+            marks += "✓"
+        check_mark.config(text=marks)
+            
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -48,7 +79,7 @@ title_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130,text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 #count_down(5)
@@ -56,10 +87,10 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", bg=YELLOW, highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset",bg=YELLOW, highlightthickness=0)
+reset_button = Button(text="Reset",bg=YELLOW, highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 
-check_mark = Label(text= "✓", fg= GREEN, bg=YELLOW)
+check_mark = Label(text="", fg= GREEN, bg=YELLOW)
 check_mark.grid(column=1, row=3)
 window.mainloop()
